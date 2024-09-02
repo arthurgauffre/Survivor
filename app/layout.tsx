82ff1ef304fee5083ab964053c11cce0b86d5cb2
Quentin Leguay <quentinleguay40@gmail.com>
2024-09-02 16:43:07 +0200

    add: component

--- app/layout.tsx
+++ app/layout.tsx
@@ -205,18 +205,8 @@ export default function RootLayout({
             </DisclosurePanel>
           </Disclosure>
 
-          <header className="bg-white shadow">
-            <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-              <h1 className="text-3xl font-bold tracking-tight text-gray-900">
-                Dashboard
-              </h1>
-            </div>
-          </header>
-          <main>
-            <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-              {children}
-            </div>
-          </main>
+          {children}
+
         </div>
       </body>
     </html>
