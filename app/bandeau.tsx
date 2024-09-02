96d8a456efe1c2047e290e0d10fca7cea229f416
Quentin Leguay <quentinleguay40@gmail.com>
2024-09-02 16:44:08 +0200

    [ADD] bandeau component

---
+++ app/bandeau.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+// import as : import SpawnBandeau from "./bandeau";
+// use as : <SpawnBandeau title="..." />
+export default function SpawnBandeau({ title } : { title: string }) {
+  return (
+    <header className="bg-white shadow">
+      <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <h1 className="text-3xl font-bold tracking-tight text-gray-900">
+          {title}
+        </h1>
+      </div>
+    </header>
+  );
+}
