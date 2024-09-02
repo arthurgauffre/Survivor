5cb453ca6df90dd39e2e1cf23263d87107d3ba39
Tom Mendy <tom.mendy@epitech.eu>
2024-09-02 17:00:47 +0200

    [FIX] SpawnHeadband

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,9 @@
-import SpawnBandeau from "./bandeau";
+import SpawnHeadband from "./SpawnHeadband";
 
 export default function Home() {
   return (
-    <div>
-      <SpawnBandeau title="Dashboard" />
-      <main>
-        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-          <h1>Home</h1>
-        </div>
-      </main>
-    </div>
+    <SpawnHeadband title="Dashboard">
+      <h1>Home</h1>
+    </SpawnHeadband>
   );
 }
