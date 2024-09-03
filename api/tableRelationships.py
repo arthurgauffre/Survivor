911adcabc07201812305b3ba6fafeda5c2fa498e
JsuisSayker <killian.trouve@epitech.eu>
2024-09-03 09:21:01 +0200

    [UPD] tableRelationships
    add the customer table

--- api/tableRelationships.py
+++ api/tableRelationships.py
@@ -26,15 +26,6 @@ class events(Base):
     location_x = Column(String, index=True)
 
 
-# Relation table between users and roles
-class user_roles(Base):
-    __tablename__ = "user_roles"
-    id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey("users.id"))
-    role_id = Column(Integer, ForeignKey("roles.id"))
-    user = relationship("User", back_populates="user_roles")
-    role = relationship("Roles", back_populates="user_roles")
-
 # Employee table
 class Employee(Base):
     __tablename__ = "employees"
@@ -48,3 +39,31 @@ class Employee(Base):
     work = Column(String, index=True)
     profilePictureLink = Column(String, index=True)
     role = relationship("Roles", back_populates="employees")
+
+
+# Customer table
+class Customer(Base):
+    __tablename__ = "customers"
+    id = Column(Integer, primary_key=True, index=True, nullable=False)
+    email = Column(String, unique=True, index=True)
+    password = Column(String, index=True)
+    name = Column(String, index=True)
+    surname = Column(String, index=True)
+    birthdate = Column(String, index=True)
+    gender = Column(String, index=True)
+    description = Column(String, index=True)
+    astrologicalSign = Column(String, index=True)
+    clothesType = Column(String, index=True)
+    payementHistory = relationship(
+        "payementHistory",
+        back_populates="customers")
+
+
+# Relation table between users and roles
+class user_roles(Base):
+    __tablename__ = "user_roles"
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    role_id = Column(Integer, ForeignKey("roles.id"))
+    user = relationship("User", back_populates="user_roles")
+    role = relationship("Roles", back_populates="user_roles")
