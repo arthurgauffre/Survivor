c15d88f8a172b89c545484eb69a209c717b5f925
Tom Mendy <tom.mendy@epitech.eu>
2024-09-03 17:26:42 +0200

    [ADD] collapse componment

--- app/tips/page.tsx
+++ app/tips/page.tsx
@@ -1,9 +1,20 @@
 import SpawnHeadband from "../SpawnHeadband";
+import Collapsible from "./collapse";
 
 export default function Home() {
   return (
     <SpawnHeadband title="Tips">
-      <div><h1> This is A TEST </h1></div>
+      <div>
+        <h1> This is A TEST </h1>
+      </div>
+
+      <div className="bg-white">
+        <Collapsible title="tkt">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia sed
+          eos vitae molestias eveniet, est quas, at, iure porro ex natus et
+          possimus numquam incidunt quisquam consequatur? Aliquid, non libero.
+        </Collapsible>
+      </div>
     </SpawnHeadband>
   );
 }
