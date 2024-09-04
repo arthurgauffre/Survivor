c9d6d3b1c09d584b8e6d2284db35cb39d9c866c6
Tom Mendy <tom.mendy@epitech.eu>
2024-09-04 10:44:15 +0200

    [ADD] crud
    adding the first crud files

--- api/routes/routes.py
+++ api/routes/routes.py
@@ -14,7 +14,7 @@ router = APIRouter()
 
 access_token = loginToken()
 
-
+###################### TEMP ROUTES THAT SEED THE DB ######################
 @router.get("/getAPIEmployeesInfos/")
 def getAPIEmployeesInfos(db: Session = Depends(get_db)):
     getAllEmployees(access_token, db)
@@ -47,3 +47,8 @@ def getAPIEncountersInfos(db: Session = Depends(get_db)):
 def getEvents(db: Session = Depends(get_db)):
     fetchingAllEvents(access_token, db)
     return {"message": "Database seeded with events"}
+###########################################################################
+
+@router.get("/api/employees/")
+def getEmployees(db: Session = Depends(get_db)):
+    return {"message": "testing the route"}
