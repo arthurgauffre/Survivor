54b36125d63f25acfa78c3dda78101029a40d600
Quentin Leguay <quentinleguay40@gmail.com>
2024-09-04 09:25:04 +0200

    [FIX] headband

--- app/SpawnHeadband.tsx
+++ app/SpawnHeadband.tsx
@@ -6,14 +6,14 @@ export default function SpawnHeadband({ title,  children } : { title: string,  c
   return (
     <>
     <header className="bg-white shadow">
-      <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+      <div className="mx-auto px-4 py-6 sm:px-6 lg:px-8">
         <h1 className="text-3xl font-bold tracking-tight text-gray-900">
           {title}
         </h1>
       </div>
     </header>
           <main>
-          <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+          <div className="mx-auto px-4 py-6 sm:px-6 lg:px-8">
             {children}
           </div>
         </main>
