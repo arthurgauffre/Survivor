6d420b47322d3707ea9b0870e8eb3d4f252ee11b
Tom Mendy <tom.mendy@epitech.eu>
2024-09-05 08:44:20 +0200

    [FIX] docker container name

--- app/component/PaymentMethod.tsx
+++ app/component/PaymentMethod.tsx
@@ -1,44 +1,52 @@
-export default function PaymentMethod({ payment }: { payment: string }): JSX.Element {
-    switch (payment) {
-      case "PayPal":
-        return (
-          <div className="flex items-center ">
-            <img
-              alt="Paypal"
-              src="https://cdn.simpleicons.org/paypal/white"
-              className="h-4 w-4 bg-[#003087]"
-            />
-          </div>
-        );
-      case "Bank Transfer":
-        return (
-          <div className="flex items-center">
-            <img
-              alt="Bank Transfer"
-              src="https://cdn.simpleicons.org/visa/white"
-              className="h-4 w-4 bg-[#1A1F71]"
-            />
-          </div>
-        );
-      case "Credit Card":
-        return (
-          <div className="flex items-center ">
-            <img
-              alt="Credit Card"
-              src="https://cdn.simpleicons.org/mastercard/white"
-              className="h-4 w-4 bg-[#EB001B]"
-            />
-          </div>
-        );
-      default:
-        return (
-          <div className="flex items-center ">
-            <img
-              alt="Paypal"
-              src="https://cdn.simpleicons.org/visa/white"
-              className="h-4 w-4 bg-[#1A1F71]"
-            />
-          </div>
-        );
-    }
+import Image from "next/image";
+
+export default function PaymentMethod({
+  payment,
+}: {
+  payment: string;
+}): JSX.Element {
+  switch (payment) {
+    case "PayPal":
+      return (
+        <div className="flex items-center ">
+          <img
+            alt="Paypal"
+            src="https://cdn.simpleicons.org/paypal/white"
+            className="h-4 w-4 bg-[#003087]"
+          />
+        </div>
+      );
+    case "Bank Transfer":
+      return (
+        <div className="flex items-center">
+          <img
+            alt="Bank Transfer"
+            src="https://cdn.simpleicons.org/visa/white"
+            className="h-4 w-4 bg-[#1A1F71]"
+          />
+        </div>
+      );
+    case "Credit Card":
+      return (
+        <div className="flex items-center bg-black">
+          <Image
+            alt="Credit Card"
+            src="https://cdn.simpleicons.org/mastercard/white"
+            width={16}
+            height={16}
+            //   className="h-4 w-4 bg-[#EB001B]"
+          />
+        </div>
+      );
+    default:
+      return (
+        <div className="flex items-center ">
+          <img
+            alt="Paypal"
+            src="https://cdn.simpleicons.org/visa/white"
+            className="h-4 w-4 bg-[#1A1F71]"
+          />
+        </div>
+      );
   }
+}
