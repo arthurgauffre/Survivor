080e4bf953c6d19c0f7dfb9b31d341fc9c4f78ea
Tom Mendy <tom.mendy@epitech.eu>
2024-09-06 12:28:18 +0200

    [UPDC] clean coaches code

--- app/coaches/[id]/edit/page.tsx
+++ app/coaches/[id]/edit/page.tsx
@@ -1,46 +1,53 @@
-import { PhotoIcon, UserCircleIcon } from '@heroicons/react/24/solid'
-import SpawnHeadband from '../../../component/SpawnHeadband'
-
+import { PhotoIcon, UserCircleIcon } from "@heroicons/react/24/solid";
+import SpawnHeadband from "../../../component/SpawnHeadband";
 
 export default function ClientProfilPage() {
   return (
     <SpawnHeadband title="Your Profile">
-    <form>
-      <div className="space-y-12">
-        <div className="border-b border-gray-900/10 pb-12">
-
-          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-            <div className="sm:col-span-3">
-              <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
-                Name
-              </label>
-              <div className="mt-2">
-                <input
-                  id="first-name"
-                  name="first-name"
-                  type="text"
-                  autoComplete="given-name"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div className="col-span-full">
-              <label htmlFor="photo" className="block text-sm font-medium leading-6 text-gray-900">
-                Photo
-              </label>
-              <div className="mt-2 flex items-center gap-x-3">
-                <UserCircleIcon aria-hidden="true" className="h-12 w-12 text-gray-300" />
-                <button
-                  type="button"
-                  className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+      <form>
+        <div className="space-y-12">
+          <div className="border-b border-gray-900/10 pb-12">
+            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+              <div className="sm:col-span-3">
+                <label
+                  htmlFor="first-name"
+                  className="block text-sm font-medium leading-6 text-gray-900"
                 >
-                  Change
-                </button>
+                  Name
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="first-name"
+                    name="first-name"
+                    type="text"
+                    autoComplete="given-name"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
               </div>
-            </div>
 
-            {/* <div className="col-span-full">
+              <div className="col-span-full">
+                <label
+                  htmlFor="photo"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Photo
+                </label>
+                <div className="mt-2 flex items-center gap-x-3">
+                  <UserCircleIcon
+                    aria-hidden="true"
+                    className="h-12 w-12 text-gray-300"
+                  />
+                  <button
+                    type="button"
+                    className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                  >
+                    Change
+                  </button>
+                </div>
+              </div>
+
+              {/* <div className="col-span-full">
               <label htmlFor="cover-photo" className="block text-sm font-medium leading-6 text-gray-900">
                 Cover photo
               </label>
@@ -61,76 +68,88 @@ export default function ClientProfilPage() {
                 </div>
               </div>
             </div> */}
+            </div>
           </div>
-        </div>
 
-        <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-          <div className="sm:col-span-3">
-            <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
-              Phone number
+          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div className="sm:col-span-3">
+              <label
+                htmlFor="first-name"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Phone number
+              </label>
+              <div className="mt-2">
+                <input
+                  id="first-name"
+                  name="first-name"
+                  type="text"
+                  autoComplete="given-name"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div className="sm:col-span-3">
+              <label
+                htmlFor="first-name"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="first-name"
+                  name="first-name"
+                  type="text"
+                  autoComplete="given-name"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="col-span-full">
+            <label
+              htmlFor="about"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Description
             </label>
             <div className="mt-2">
-              <input
-                id="first-name"
-                name="first-name"
-                type="text"
-                autoComplete="given-name"
+              <textarea
+                id="about"
+                name="about"
+                rows={3}
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                defaultValue={""}
               />
             </div>
           </div>
-        </div>
 
-        <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-          <div className="sm:col-span-3">
-            <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
-              Address
-            </label>
-            <div className="mt-2">
-              <input
-                id="first-name"
-                name="first-name"
-                type="text"
-                autoComplete="given-name"
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
+          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div className="sm:col-span-3">
+              <label
+                htmlFor="first-name"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Payment history
+              </label>
+              <div className="mt-2">
+                <input
+                  id="first-name"
+                  name="first-name"
+                  type="text"
+                  autoComplete="given-name"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
             </div>
           </div>
         </div>
-
-        <div className="col-span-full">
-          <label htmlFor="about" className="block text-sm font-medium leading-6 text-gray-900">
-            Description
-          </label>
-          <div className="mt-2">
-            <textarea
-              id="about"
-              name="about"
-              rows={3}
-              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              defaultValue={''}
-            />
-          </div>
-        </div>
-
-        <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-          <div className="sm:col-span-3">
-            <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
-              Payment history
-            </label>
-            <div className="mt-2">
-              <input
-                id="first-name"
-                name="first-name"
-                type="text"
-                autoComplete="given-name"
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-    </form>
+      </form>
     </SpawnHeadband>
-  )
+  );
 }
--- app/coaches/[id]/page.tsx
+++ app/coaches/[id]/page.tsx
@@ -1,158 +1,125 @@
 import "./../../component/table.css";
 import React from "react";
 import {
-    EnvelopeIcon,
-    BookmarkIcon,
-    StarIcon,
-    ArrowLeftIcon,
+  EnvelopeIcon,
+  BookmarkIcon,
+  StarIcon,
+  ArrowLeftIcon,
 } from "@heroicons/react/24/outline";
 import { StarIcon as BlackStarIcon } from "@heroicons/react/24/solid";
 import SpawnHeadband from "../../component/SpawnHeadband";
 import CoachesChartEventsStatistics from "@/app/component/coachesChartEventsStatistics";
 import CoachesRatingsChart from "@/app/component/coachesRatingsChart";
 
-const meetings = [
-    {
-        id: 1,
-        customer_id: 17,
-        date: "2023-12-21",
-        rating: 2,
-        comment: "A very good moment!",
-        source: "school",
-    },
-    {
-        id: 2,
-        customer_id: 17,
-        date: "2023-12-21",
-        rating: 2,
-        comment: "A very good moment!",
-        source: "school",
-    },
-    {
-        id: 3,
-        customer_id: 17,
-        date: "2023-12-21",
-        rating: 2,
-        comment: "A very good moment!",
-        source: "school",
-    },
-    {
-        id: 4,
-        customer_id: 17,
-        date: "2023-12-21",
-        rating: 2,
-        comment: "A very good moment!",
-        source: "school",
-    },
-    {
-        id: 5,
-        customer_id: 17,
-        date: "2023-12-21",
-        rating: 2,
-        comment: "A very good moment!",
-        source: "school",
-    },
-];
-
-
-
 const Links = {
-    customers_id: [17]
+  customers_id: [17],
 };
 
 function Rating(numberStar: number) {
-    const rating = [];
+  const rating = [];
 
-    for (let i = 0; i < numberStar; i++) {
-        rating.push(<BlackStarIcon key={i} className="h-4 w-4" />);
-    }
-    for (let i = numberStar; i < 5; i++) {
-        rating.push(<StarIcon key={i} className="h-4 w-4" />);
-    }
-    return rating;
+  for (let i = 0; i < numberStar; i++) {
+    rating.push(<BlackStarIcon key={i} className="h-4 w-4" />);
+  }
+  for (let i = numberStar; i < 5; i++) {
+    rating.push(<StarIcon key={i} className="h-4 w-4" />);
+  }
+  return rating;
 }
 
-export default async function Page({ params }: { params: { id: string } }): Promise<JSX.Element> {
-    let dataEmployees = await fetch('http://fastapi:8000/api/employees/' + params.id);
-    let postsEmployees = await dataEmployees.json();
+export default async function Page({
+  params,
+}: {
+  params: { id: string };
+}): Promise<JSX.Element> {
+  let dataEmployees = await fetch(
+    "http://fastapi:8000/api/employees/" + params.id
+  );
+  let postsEmployees = await dataEmployees.json();
 
-    let dataImg = await fetch('http://fastapi:8000/api/employees/' + params.id + '/image');
-    let Img = await dataImg.json();
-    const imgUrl: string = Img.image_url;
+  let dataImg = await fetch(
+    "http://fastapi:8000/api/employees/" + params.id + "/image"
+  );
+  let Img = await dataImg.json();
+  const imgUrl: string = Img.image_url;
 
-    let dataEvents = await fetch('http://fastapi:8000/api/events/' + params.id);
-    let postsEvents = await dataEvents.json();
+  let dataEvents = await fetch("http://fastapi:8000/api/events/" + params.id);
+  let postsEvents = await dataEvents.json();
 
-    let ratingMeetings: number[] = [0, 0, 0, 0, 0];
-    for (const customerId of Links.customers_id) {
-        let dataRatings = await fetch('http://fastapi:8000/api/encounters/customer/' + customerId);
-        let postsRatings = await dataRatings.json();
-        for (const postRating of postsRatings) {
-            ratingMeetings[postRating.rating - 1] += 1;
-        }
-    }
-    console.log(ratingMeetings);
-
-    return (
-        <SpawnHeadband
-            title='Coach Profile'
-            elemRight={
-                <div className="flex">
-                    <a href="/coaches">
-                        <button className="ml-4 bg-white text-[#2263b3] py-2 px-2 rounded text-sm flex items-center">
-                            <ArrowLeftIcon className="h-6 w-6 mr-2" />
-                            <p>Back</p>
-                        </button>
-                    </a>
-                </div>
-            }
-        >
-            <div className="sm:flex sm:flex-row gap-2">
-                <div className="sm:flex-col border bg-white rounded-md">
-                    <div className="sm:flex flex-col items-center justify-center text-center border-b p-2">
-                        <img
-                            alt="Image of user"
-                            src={imgUrl}
-                            className="w-14 rounded-full"
-                        />
-                        <p className="mt-2">{postsEmployees.name} {postsEmployees.surname}</p> {/* Added margin-top to separate text from the image */}
-                    </div>
-                    <div className="border-b p-2">
-                        <p>short detail</p>
-                        <br />
-                        <p className="text-gray-500">Email:</p>
-                        <p>{postsEmployees.email}</p>
-                        <br />
-                        <p className="text-gray-500">Birthdate:</p>
-                        <p>{postsEmployees.birthdate}</p>
-                        <br />
-                        <p className="text-gray-500">Gender:</p>
-                        <p>{postsEmployees.gender}</p>
-                        <br />
-                        <p className="text-gray-500">Work:</p>
-                        <p>{postsEmployees.work}</p>
-                        <br />
-                        <p className="text-gray-500">Number of clients:</p>
-                        <p>{Links.customers_id.length}</p>
-                    </div>
-                </div>
-                <div className="sm:flex sm:flex-wrap sm:w-full gap-2">
-                    <div className="sm:w-full bg-white p-2 rounded-md">
-                        <div className="md:w-1/2 max-h-80">
-                            <CoachesChartEventsStatistics data={postsEvents} />
-                        </div>
-                    </div>
-                    <div className="sm:w-full bg-white p-2 rounded-md max-sm:my-1">
-                        <div className="md:w-1/3 max-h-80">
-                            <CoachesRatingsChart data={ratingMeetings} />
-                        </div>
-                    </div>
-                    <div className="sm:w-full bg-white p-2 rounded-md">
-                        <div className="md:w-1/2 max-h-80">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </SpawnHeadband>
+  let ratingMeetings: number[] = [0, 0, 0, 0, 0];
+  for (const customerId of Links.customers_id) {
+    let dataRatings = await fetch(
+      "http://fastapi:8000/api/encounters/customer/" + customerId
     );
+    let postsRatings = await dataRatings.json();
+    for (const postRating of postsRatings) {
+      ratingMeetings[postRating.rating - 1] += 1;
+    }
+  }
+  console.log(ratingMeetings);
+
+  return (
+    <SpawnHeadband
+      title="Coach Profile"
+      elemRight={
+        <div className="flex">
+          <a href="/coaches">
+            <button className="ml-4 bg-white text-[#2263b3] py-2 px-2 rounded text-sm flex items-center">
+              <ArrowLeftIcon className="h-6 w-6 mr-2" />
+              <p>Back</p>
+            </button>
+          </a>
+        </div>
+      }
+    >
+      <div className="sm:flex sm:flex-row gap-2">
+        <div className="sm:flex-col border bg-white rounded-md">
+          <div className="sm:flex flex-col items-center justify-center text-center border-b p-2">
+            <img
+              alt="Image of user"
+              src={imgUrl}
+              className="w-14 rounded-full"
+            />
+            <p className="mt-2">
+              {postsEmployees.name} {postsEmployees.surname}
+            </p>{" "}
+            {/* Added margin-top to separate text from the image */}
+          </div>
+          <div className="border-b p-2">
+            <p>short detail</p>
+            <br />
+            <p className="text-gray-500">Email:</p>
+            <p>{postsEmployees.email}</p>
+            <br />
+            <p className="text-gray-500">Birthdate:</p>
+            <p>{postsEmployees.birthdate}</p>
+            <br />
+            <p className="text-gray-500">Gender:</p>
+            <p>{postsEmployees.gender}</p>
+            <br />
+            <p className="text-gray-500">Work:</p>
+            <p>{postsEmployees.work}</p>
+            <br />
+            <p className="text-gray-500">Number of clients:</p>
+            <p>{Links.customers_id.length}</p>
+          </div>
+        </div>
+        <div className="sm:flex sm:flex-wrap sm:w-full gap-2">
+          <div className="sm:w-full bg-white p-2 rounded-md">
+            <div className="md:w-1/2 max-h-80">
+              <CoachesChartEventsStatistics data={postsEvents} />
+            </div>
+          </div>
+          <div className="sm:w-full bg-white p-2 rounded-md max-sm:my-1">
+            <div className="md:w-1/3 max-h-80">
+              <CoachesRatingsChart data={ratingMeetings} />
+            </div>
+          </div>
+          <div className="sm:w-full bg-white p-2 rounded-md">
+            <div className="md:w-1/2 max-h-80"></div>
+          </div>
+        </div>
+      </div>
+    </SpawnHeadband>
+  );
 }
