346d5c8b59c07d9ffdd2998c74ae6714085e85e5
Quentin Leguay <quentinleguay40@gmail.com>
2024-09-06 11:29:18 +0200

    [MOD] search bar in clothes page

--- app/clothes/page.tsx
+++ app/clothes/page.tsx
@@ -29,17 +29,77 @@ const images = [
   },
 ];
 
-export default async function Home() {
-  let customers = [];
-  let iages = []
-  try {
-    let data = await fetch('http://fastapi:8000/api/customers');
-    customers = await data.json();
-    data = await fetch('http://fastapi:8000/api/images');
-    iages = await data.json();
-  } catch (error) {
-    console.error("Error fetching data:", error);
+const customers = [
+  {
+    id: 1,
+    email: "john.doe@example.com",
+    password: "password123",
+    name: "John",
+    surname: "Doe",
+    birthdate: "1990-05-15",
+    gender: "Male",
+    description: "Avid traveler and photography enthusiast.",
+    astrologicalSign: "Taurus",
+    birth_date: "1990-05-15",
+    phone_number: "+1234567890",
+    address: "123 Elm Street, Springfield"
+  },
+  {
+    id: 2,
+    email: "jane.smith@example.com",
+    password: "securePass!56",
+    name: "Jane",
+    surname: "Smith",
+    birthdate: "1988-10-25",
+    gender: "Female",
+    description: "Tech geek and software developer.",
+    astrologicalSign: "Scorpio",
+    birth_date: "1988-10-25",
+    phone_number: "+0987654321",
+    address: "456 Maple Avenue, Rivertown"
+  },
+  {
+    id: 3,
+    email: "alex.johnson@example.com",
+    password: "Alex@2024",
+    name: "Alex",
+    surname: "Johnson",
+    birthdate: "1995-03-08",
+    gender: "Non-binary",
+    description: "Loves painting and music production.",
+    astrologicalSign: "Pisces",
+    birth_date: "1995-03-08",
+    phone_number: "+1122334455",
+    address: "789 Oak Road, Greenfield"
+  },
+  {
+    id: 4,
+    email: "emma.brown@example.com",
+    password: "Emma#Secure",
+    name: "Emma",
+    surname: "Brown",
+    birthdate: "1992-07-12",
+    gender: "Female",
+    description: "Fitness enthusiast and blogger.",
+    astrologicalSign: "Cancer",
+    birth_date: "1992-07-12",
+    phone_number: "+3344556677",
+    address: "101 Pine Street, Bluetown"
   }
+];
+
+
+export default async function Home() {
+  // let customers = [];
+  // let images = []
+  // try {
+  //   let data = await fetch('http://fastapi:8000/api/customers');
+  //   customers = await data.json();
+  //   data = await fetch('http://fastapi:8000/api/images');
+  //   iages = await data.json();
+  // } catch (error) {
+  //   console.error("Error fetching data:", error);
+  // }
 
   return (
     <SpawnHeadband title="Clothes" littleText="Customize your drip">
