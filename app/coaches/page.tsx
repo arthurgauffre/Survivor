454360909dfd0375259c69e466992902d80a5ac5
Tom Mendy <tom.mendy@epitech.eu>
2024-09-06 10:15:06 +0200

    [ADD] clickable rows customers coaches

--- app/coaches/page.tsx
+++ app/coaches/page.tsx
@@ -1,19 +1,22 @@
 import CoachesTable from "./coachesTable";
 
 export default async function Home() {
-  let data = await fetch("http://fastapi:8000/api/employees");
-  let coaches: {
-    id: number;
-    email: string;
-    name: string;
-    surname: string;
-    birthdate: string;
-    gender: string;
-    description: string;
-    astrologicalSign: string;
-  }[] = await data.json();
+  try {
+    let data = await fetch("http://fastapi:8000/api/employees");
+    let coaches: {
+      id: number;
+      email: string;
+      name: string;
+      surname: string;
+      birthdate: string;
+      gender: string;
+      description: string;
+      astrologicalSign: string;
+    }[] = await data.json();
+    return <CoachesTable coaches={coaches} />;
+  } catch (e) {
+    return <CoachesTable coaches={[]} />;
+    console.log(e);
+  }
 
-  return (
-    <CoachesTable coaches={coaches} />
-  );
 }
