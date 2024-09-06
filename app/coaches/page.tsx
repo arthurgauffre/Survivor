919e65901898ce399a9f18dd4a7a044ef45e15c9
Tom Mendy <tom.mendy@epitech.eu>
2024-09-06 09:48:44 +0200

    [FIX] coaches detail redirection

--- app/coaches/page.tsx
+++ app/coaches/page.tsx
@@ -1,137 +1,20 @@
-import "../component/table.css";
-import SpawnHeadband from "../component/SpawnHeadband";
-import InputRequest from "./inputRequest";
-import DropdownMenu from "../component/DropdownMenu";
-import CheckBoxAll from "../component/CheckBoxAll";
-import {
-  MagnifyingGlassIcon,
-  AdjustmentsHorizontalIcon,
-  Cog8ToothIcon,
-  PlusIcon,
-  CloudArrowDownIcon,
-  EllipsisHorizontalIcon,
-} from "@heroicons/react/20/solid";
-import { MenuButton, Menu, MenuItem, MenuItems } from "@headlessui/react";
-
-const ActionsActions = [
-  { name: "Edit", href: "coaches", class: "" },
-  { name: "Delete", href: "#", class: " text-red-500" },
-];
+import CoachesTable from "./coachesTable";
 
 export default async function Home() {
-  let data = await fetch('http://fastapi:8000/api/employees');
-  let coaches = await data.json();
+  let data = await fetch("http://fastapi:8000/api/employees");
+  let coaches: {
+    id: number;
+    email: string;
+    name: string;
+    surname: string;
+    birthdate: string;
+    gender: string;
+    description: string;
+    astrologicalSign: string;
+  }[] = await data.json();
   let littletext = "You have total of " + coaches.length + " coaches";
 
   return (
-    <SpawnHeadband
-      title="Coaches"
-      littleText={littletext}
-      elemRight={
-        <div className="flex">
-          <button className="ml-4 bg-white text-[#2263b3] py-2 px-2 rounded text-sm flex items-center">
-            <CloudArrowDownIcon className="h-6 w-6 mr-2"></CloudArrowDownIcon>
-            <p>Export</p>
-          </button>
-          <button className="ml-4 bg-[#2263b3] text-white py-2 px-2 rounded text-sm">
-            <PlusIcon className="h-6 w-6"></PlusIcon>
-          </button>
-        </div>
-      }
-    >
-      <div style={{ color: "Black" }}>
-        <ul role="list" className="divide-y-2 divide-gray-100">
-          <li className="flex gap-x-6 py-5 justify-between border-1 bg-white rounded-t-md px-4">
-            <div className="flex">
-              <DropdownMenu
-                title="Bulk Action"
-                elements={["thing one", "thing two"]}
-              />
-              <button className="ml-4 bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded text-sm">
-                Apply
-              </button>
-            </div>
-            <div className="flex">
-              {/* <InputRequest title="Search" placeholderString="Search" /> */}
-              <div className="flex items-center border-r-2">
-                <MagnifyingGlassIcon className="h-6 text-gray-400 px-2" />
-              </div>
-            </div>
-          </li>
-
-          <li>
-            <table className="bg-white">
-              <thead>
-                <tr>
-                  <th className="p-2">
-                    <CheckBoxAll />
-                  </th>
-                  <th>Coach</th>
-                  <th>Email</th>
-                  <th>Phone</th>
-                  <th>Number of customers</th>
-                  <th align="right" className="pr-4">Actions</th>
-                </tr>
-              </thead>
-              <tbody>
-                {coaches.map((person) => (
-                  <tr key={person.id}>
-                    <td className="p-2">
-                      <input className="RowBox" type="checkbox" />
-                    </td>
-                    <td>
-                      <span className="cell-header">Coach:</span>
-                      <div className="flex items-center">
-                        <img
-                          alt=""
-                          src="https://media.tenor.com/6uPPCdKYocAAAAAe/panik-kalm.png"
-                          className="h-12 w-12 flex-none rounded-full bg-gray-50 mr-4 my-1"
-                        />
-                        {person.name} {person.surname}
-                      </div>
-                    </td>
-                    <td>
-                      <span className="cell-header">Email:</span>
-                      {person.email}
-                    </td>
-                    <td>
-                      <span className="cell-header">Phone:</span>
-                      {person.birth_date}
-                    </td>
-                    <td>
-                      <span className="cell-header">Number of customers:</span>
-                      {person.birth_date}
-                    </td>
-                    <td align="right" className="pr-6">
-                      <span className="cell-header">Actions:</span>
-                      <Menu>
-                        <MenuButton className="relative flex max-w-xs items-center">
-                          <EllipsisHorizontalIcon className="h-6 text-gray-400 px-2"></EllipsisHorizontalIcon>
-                        </MenuButton>
-                        <MenuItems
-                          transition
-                          className="absolute right-8 z-10 mt-2 px-2 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
-                        >
-                          {ActionsActions.map((item) => (
-                            <MenuItem key={item.name}>
-                              <a
-                                href={item.href + "/" + person.id}
-                                className={"block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100" + item.class}
-                              >
-                                {item.name}
-                              </a>
-                            </MenuItem>
-                          ))}
-                        </MenuItems>
-                      </Menu>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </li>
-        </ul>
-      </div>
-    </SpawnHeadband>
+    <CoachesTable coaches={coaches} />
   );
 }
