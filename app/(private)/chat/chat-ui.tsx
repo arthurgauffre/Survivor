b5f3df196d75e58832960a57eceefbb611f01697
Tom Mendy <tom.mendy@epitech.eu>
2024-09-11 15:00:38 +0200

    [ADD] useEffect

--- app/(private)/chat/chat-ui.tsx
+++ app/(private)/chat/chat-ui.tsx
@@ -5,33 +5,7 @@ import { useFormState, useFormStatus } from "react-dom";
 import { ArrowLeftIcon, PaperAirplaneIcon } from "@heroicons/react/24/outline";
 import { SubmitMessage } from "@/app/(private)/chat/submitMessage";
 import Image from "next/image";
-
-export function LeftMessage({
-  image,
-  text,
-}: {
-  image: string;
-  text: string;
-}): JSX.Element {
-  return (
-    <div className="flex items-end">
-      <img className="h-8 w-8 rounded-full" src={image} alt="Contact Picture" />
-      <div className="ml-2 bg-gray-200 rounded-lg p-3 max-w-xs">
-        <p className="text-sm">{text}</p>
-      </div>
-    </div>
-  );
-}
-
-export function RightMessage({ text }: { text: string }): JSX.Element {
-  return (
-    <div className="flex items-end justify-end">
-      <div className="mr-2 bg-blue-500 text-white rounded-lg p-3 max-w-xs">
-        <p className="text-sm">{text}</p>
-      </div>
-    </div>
-  );
-}
+import { MainChat } from "@/app/(private)/chat/main-chat";
 
 export function LeftContact({
   contacts,
@@ -55,101 +29,19 @@ export function LeftContact({
   );
 }
 
-export function MainChat({
-  selectedContact,
-  toggleSidebar,
-}: {
-  selectedContact: number;
-  toggleSidebar: () => void;
-}): JSX.Element {
-  const [state, action] = useFormState(SubmitMessage, undefined);
-  const [message, setMessage] = useState("");
-
-  return (
-    <>
-      {/* Chat Header */}
-      <header className="bg-white border-b p-4 flex items-center justify-between">
-        <button className="md:hidden" onClick={toggleSidebar}>
-          <ArrowLeftIcon className="h-6 w-6" />
-          <span className="sr-only">Toggle sidebar</span>
-        </button>
-        <div className="flex items-center">
-          <img
-            className="h-10 w-10 rounded-full"
-            src={`https://i.pravatar.cc/40?img=${selectedContact}`}
-            alt="Alice"
-          />
-          {/* <h1 className="ml-3 text-xl font-semibold">
-            {contacts.find((contact) => contact.id === selectedContact)?.name}
-          </h1> */}
-        </div>
-      </header>
-      {/* Chat Messages */}
-      <div className="flex-1 overflow-y-auto p-4 space-y-4">
-        <LeftMessage
-          image={`https://i.pravatar.cc/40?img=${selectedContact}`}
-          text="Hey, how are you doing?"
-        />
-        <RightMessage text="I'm good, thanks! How about you?" />
-        <LeftMessage
-          image={`https://i.pravatar.cc/40?img=${selectedContact}`}
-          text="lol, I'm good too!"
-        />
-      </div>
-      {/* Message Input */}
-      <div className="bg-white border-t p-4">
-        <form className="flex space-x-2" action={action}>
-          <input
-            type="text"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            className="flex h-9 w-full rounded-md border border-neutral-200 bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-neutral-950 disabled:cursor-not-allowed disabled:opacity-50 dark:border-neutral-800 dark:placeholder:text-neutral-400 dark:focus-visible:ring-neutral-300"
-            placeholder="Type a message ..."
-          />
-          <SendButton setMessage={setMessage} />
-        </form>
-      </div>
-    </>
-  );
-}
-
-export function SendButton({
-  setMessage,
-}: {
-  setMessage: (message: string) => void;
-}): JSX.Element {
-  const { pending } = useFormStatus();
-
-  return (
-    <button
-      aria-disabled={pending}
-      type="submit"
-      className="bg-blue-500 rounded-md text-white"
-      onClick={() => setMessage("")}
-    >
-      {pending ? (
-        "Submitting..."
-      ) : (
-        <>
-          <PaperAirplaneIcon className="h-5 w-10 " />
-          <span className="sr-only">Send message</span>
-        </>
-      )}
-    </button>
-  );
-}
-
 export function ChatUi({
   contacts,
+  accessToken
 }: {
   contacts: {
     contact_id: number;
-    message: string;
-    date: string;
-    senderId: number;
+    message: string | undefined;
+    date: string | undefined;
+    senderId: number | undefined;
     name: string;
     image: string;
   }[];
+  accessToken: string;
 }) {
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const [selectedContact, setSelectedContact] = useState(0);
@@ -182,8 +74,8 @@ export function ChatUi({
                 contacts={{
                   id: contact.contact_id,
                   name: contact.name,
-                  lastMessage: contact.message,
-                  image: contact.image
+                  lastMessage: contact.message || "",
+                  image: contact.image,
                 }}
               />
             </button>
@@ -198,6 +90,17 @@ export function ChatUi({
         <MainChat
           selectedContact={selectedContact}
           toggleSidebar={toggleSidebar}
+          contact={{
+            contact_id: (contacts.find((contact) => contact.contact_id === selectedContact)
+            ?.contact_id) || 1,
+            senderId: (contacts.find((contact) => contact.contact_id === selectedContact)
+            ?.senderId),
+            name: (contacts.find((contact) => contact.contact_id === selectedContact)
+            ?.name) || "",
+            image: (contacts.find((contact) => contact.contact_id === selectedContact
+            )?.image) || ""
+          }}
+          accessToken={accessToken}
         />
       </div>
     </div>
--- app/(private)/chat/page.tsx
+++ app/(private)/chat/page.tsx
@@ -13,10 +13,36 @@ export default async function Page() {
   const userRole = session?.role;
   const accessToken: string = session?.accessToken;
 
+  switch (userRole) {
+    case "admin":
+      redirect("/dashboard");
+    case "coach":
+      return await CoachesPage({ accessToken, userId: session.userId });
+    case "customer":
+      return await CustomerPage({ accessToken, userId: session.userId });
+    default:
+      redirect("/login");
+  }
+}
+
+export async function CustomerPage({
+  accessToken,
+  userId,
+}: {
+  accessToken: string;
+  userId: number;
+}) {
   const contactData = await customFetch(
     "http://fastapi:8000/api/chat",
     accessToken
   );
+
+  const userData = await customFetch(
+    "http://fastapi:8000/api/customers" + userId,
+    accessToken
+  );
+  const user = userData.json();
+
   const contactsReceive: {
     customer_id: number;
     employee_id: number;
@@ -25,65 +51,56 @@ export default async function Page() {
     senderId: number;
   }[] = await contactData.json();
 
-  const contactsTMP: {
+  let contactsList: {
     contact_id: number;
     message: string;
     date: string;
     senderId: number;
-  }[] = contactsReceive.map((contact) => ({
-    contact_id:
-      userRole === "customer" ? contact.employee_id : contact.customer_id,
-    message: contact.message,
-    date: contact.date,
-    senderId: contact.senderId,
-  }));
+  }[] = [];
+  if (contactsReceive[0] !== undefined) {
+    contactsList = contactsReceive.map((contact) => ({
+      contact_id: contact.employee_id,
+      message: contact.message,
+      date: contact.date,
+      senderId: contact.senderId,
+    }));
+  } else {
+    // contactsList = {
+    //   contact_id: user,
+    //   message: undefined,
+    //   date: undefined,
+    //   senderId: undefined,
+    // };
+  }
 
   let personalData: {
     id: number;
     name: string;
     image: string;
   }[] = [];
-  for (const contact of contactsTMP) {
+
+  for (const contact of contactsList) {
     let contactSum: {
       id: number;
       name: string;
       image: string;
     };
-    if (userRole == "customer") {
-      const contactData = await customFetch(
-        `http://fastapi:8000/api/employees/${contact.contact_id}`,
-        accessToken
-      );
-      const contactInfo = await contactData.json();
-      const imageData = await customFetch(
-        `http://fastapi:8000/api/employees/${contact.contact_id}/image`,
-        accessToken
-      );
-      const image = await imageData.json();
+    const contactData = await customFetch(
+      `http://fastapi:8000/api/employees/${contact.contact_id}`,
+      accessToken
+    );
+    const contactInfo = await contactData.json();
+    const imageData = await customFetch(
+      `http://fastapi:8000/api/employees/${contact.contact_id}/image`,
+      accessToken
+    );
+    const image = await imageData.json();
 
-      contactSum = {
-        id: contact.contact_id,
-        name: contactInfo.name,
-        image: image,
-      };
-    } else {
-      const contactData = await customFetch(
-        `http://fastapi:8000/api/employees/${contact.contact_id}`,
-        accessToken
-      );
-      const contactInfo = await contactData.json();
-      const imageData = await customFetch(
-        `http://fastapi:8000/api/employees/${contact.contact_id}/image`,
-        accessToken
-      );
-      const image = await imageData.json();
-
-      contactSum = {
-        id: contact.contact_id,
-        name: contactInfo.name,
-        image: image,
-      };
-    }
+    contactSum = {
+      id: contact.contact_id,
+      name: contactInfo.name,
+      image: image,
+    };
     personalData.push(contactSum);
   }
 
@@ -94,7 +111,7 @@ export default async function Page() {
     senderId: number;
     name: string;
     image: string;
-  }[] = contactsTMP.map((contact) => ({
+  }[] = contactsList.map((contact) => ({
     contact_id: contact.contact_id,
     message: contact.message,
     date: contact.date,
@@ -107,14 +124,135 @@ export default async function Page() {
         ?.image || "",
   }));
 
-  switch (userRole) {
-    case "admin":
-      return <ChatUi contacts={contacts} />;
-    case "user":
-      redirect("/dashboard");
-    case "coach":
-      return <ChatUi contacts={contacts} />;
-    default:
-      redirect("/login");
+  return <ChatUi contacts={contacts} accessToken={accessToken} />;
+}
+
+export async function CoachesPage({
+  accessToken,
+  userId,
+}: {
+  accessToken: string;
+  userId: number;
+}) {
+  const contactData = await customFetch(
+    "http://fastapi:8000/api/chat/",
+    accessToken
+  );
+
+  const userData = await customFetch(
+    "http://fastapi:8000/api/employees/" + userId,
+    accessToken
+  );
+  const user: {
+    id: number;
+    email: string;
+    name: string;
+    surname: string;
+    birthdate: string;
+    gender: string;
+    work: string;
+    customer_list: number[];
+  } = await userData.json();
+
+  const contactsReceive: {
+    customer_id: number;
+    employee_id: number;
+    message: string;
+    date: string;
+    senderId: number;
+  }[] = await contactData.json();
+
+  let contactsList: {
+    contact_id: number;
+    message: string | undefined;
+    date: string | undefined;
+    senderId: number | undefined;
+  }[] = [];
+
+  user.customer_list.map((customer) => {
+    contactsList.push({
+      contact_id: customer,
+      message: undefined,
+      date: undefined,
+      senderId: undefined,
+    });
+  });
+
+  if (contactsReceive[0] !== undefined) {
+    // contactsReceive.map((contact) => ({
+    //   contactsList.filter((customer) => (
+    //     customer.customer_id.includes(contact.customer_id)
+    //   ))[0].message = contact.message,
+    //   contactsList.filter((customer) => (
+    //     customer.customer_id.includes(contact.customer_id)
+    //   ))[0].date = contact.date,
+    //   contactsList.filter((customer) => (
+    //     customer.customer_id.includes(contact.customer_id)
+    //   ))[0].senderId = contact.senderId
+    // }));
   }
+
+  let personalData: {
+    id: number;
+    name: string;
+    image: string;
+  }[] = [];
+
+  for (const contact of contactsList) {
+    let contactSum: {
+      id: number;
+      name: string;
+      image: string;
+    };
+    const contactData = await customFetch(
+      `http://fastapi:8000/api/customers/${contact.contact_id}`,
+      accessToken
+    );
+    const contactInfo: {
+      id: number;
+      email: string;
+      name: string;
+      surname: string;
+      birthdate: string;
+      gender: string;
+      description: string;
+      astrologicalSign: string;
+      phone_number: string;
+      address: string;
+    } = await contactData.json();
+    const imageData = await customFetch(
+      `http://fastapi:8000/api/customers/${contact.contact_id}/image`,
+      accessToken
+    );
+    const image: string = await imageData.json();
+
+    contactSum = {
+      id: contact.contact_id,
+      name: contactInfo.name + " " + contactInfo.surname,
+      image: image,
+    };
+    personalData.push(contactSum);
+  }
+
+  const contacts: {
+    contact_id: number;
+    message: string | undefined;
+    date: string | undefined;
+    senderId: number | undefined;
+    name: string;
+    image: string;
+  }[] = contactsList.map((contact) => ({
+    contact_id: contact.contact_id,
+    message: contact.message,
+    date: contact.date,
+    senderId: contact.senderId,
+    name:
+      personalData.find((personal) => personal.id == contact.contact_id)
+        ?.name || "Name",
+    image:
+      personalData.find((personal) => personal.id == contact.contact_id)
+        ?.image || "Image",
+  }));
+
+  return <ChatUi contacts={contacts} accessToken={accessToken} />;
 }
