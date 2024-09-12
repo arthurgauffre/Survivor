4ebf10fd447847e13c2b96d1a6838c75ae45582b
JsuisSayker <killian.trouve@epitech.eu>
2024-09-12 10:18:07 +0200

    [UPD] customerSchema
    add | None to some args to ensure that the return value does not create an error

--- api/schemas/customerSchemas.py
+++ api/schemas/customerSchemas.py
@@ -3,15 +3,15 @@ from pydantic import BaseModel
 
 class CustomerBasicSchema(BaseModel):
     id: int
-    email: str
-    name: str
-    surname: str
-    birthdate: str
-    gender: str
-    description: str
-    astrologicalSign: str
-    phone_number: str
-    address: str
+    email: str | None
+    name: str | None
+    surname: str | None
+    birthdate: str | None
+    gender: str | None
+    description: str | None
+    astrologicalSign: str | None
+    phone_number: str | None
+    address: str  | None
 
 
 class CustomerWithCoachSchema(CustomerBasicSchema):
